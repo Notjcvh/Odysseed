45f18e0e8a5d708bd79c7548eae7e77671bd1be1
Sterling Stokes <sstokes4@gmu.edu>
2022-10-25 01:16:36 -0400

    Last push to the Attack Script
    Most Important thing to know is I created a new Layer Mask called whatIsHittable. So this can be enemies or boxes, etc

--- My project/Assets/PlayerAttack.cs
+++ My project/Assets/PlayerAttack.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timeBetweenAttacks;
+    public float startTimeBetweenAttacks;
+
+
+    public Transform attackPosition;
+
+    public LayerMask whatIsHittable;
+    public float attackRadius;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (timeBetweenAttacks <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Debug.Log("Attack Button Pressed");
+                //  Collider[] enemiestoDamage = Physics.OverlapSphere(attackPosition.position, attackRange, whatIsHittable);
+            }
+
+            timeBetweenAttacks = startTimeBetweenAttacks;
+        }
+        else
+            timeBetweenAttacks -= Time.deltaTime;
+
+
     }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(attackPosition.position, attackRadius);
+    }
+
 }
