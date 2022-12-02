2a5cb75b14ea84464c20b641ede3beac09a2b617
Notjcvh <ihong4@gmu.edu>
2022-12-02 17:55:28 -0500

    Boss Update

--- My project/Assets/Isaac's Test/Scripts/BossAi.cs
+++ My project/Assets/Isaac's Test/Scripts/BossAi.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class BossAi : MonoBehaviour
 {
+    [Header("Generic Attributes")]
+    public float genericAtkSpeed;
+    private float genericAtkSpeedCounter;
+    public int bossHealth;
+    public int meleeAtkRange;
+    public int nextAttack;
     [Header("Attack1 Attributes")]
     public float attack1Speed;
     public GameObject attack1;
     public float attack1Life;
+    [Header("Attack2 Attributes")]
+    public float attack2Speed;
+    public GameObject[] spawnLocations;
+    public GameObject attack2;
+    public float attack2Life;
     [Header("Summon Enemy Attributes")]
     public float summonEnemySpeed;
     public GameObject enemy;
@@ -19,14 +30,43 @@ public class BossAi : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        Attack1();
-        SummonEnemyAtack();
     }
 
     // Update is called once per frame
     void Update()
     {
         distanceFromPlayer = Vector3.Distance(this.transform.position, player.transform.position);
+        genericAtkSpeedCounter -= Time.deltaTime;
+        if(genericAtkSpeedCounter <= 0)
+        {
+            if (distanceFromPlayer >= meleeAtkRange)
+            {
+                if (nextAttack != 1)
+                    nextAttack = 1;
+                else
+                    nextAttack = 3;
+            }
+            else
+            {
+                if (nextAttack != 2)
+                    nextAttack = 2;
+                else
+                    nextAttack = 3;
+            }
+            if(nextAttack == 1)
+            {
+                Attack1();
+            }
+            else if (nextAttack == 2)
+            {
+                Attack2();
+            }
+            else
+            {
+                SummonEnemyAtack();
+            }
+            genericAtkSpeedCounter = genericAtkSpeed;
+        }
     }
 
     public void Attack1()
@@ -34,6 +74,16 @@ public class BossAi : MonoBehaviour
         Invoke("SpawnAttack1", attack1Speed);
         Invoke("SpawnAttack1", attack1Speed*2);
         Invoke("SpawnAttack1", attack1Speed*3);
+    }
+
+    public void Attack2()
+    {
+        foreach (var item in spawnLocations)
+        {
+            Vector3 spawnLocation = item.transform.position;
+            SpawnAttack2(spawnLocation);
+        }
+        
     }
 
     public void SummonEnemyAtack()
@@ -49,9 +99,15 @@ public class BossAi : MonoBehaviour
         GameObject inGameAttack1 = Instantiate(attack1, spawnLocation, player.transform.rotation);
         Destroy(inGameAttack1, attack1Life);
     }
-
+    public void SpawnAttack2(Vector3 spawnLocation)
+    {
+        GameObject inGameAttack1 = Instantiate(attack1, spawnLocation, player.transform.rotation);
+        Destroy(inGameAttack1, attack2Life);
+    }
     public void SummonEnemy()
     {
         GameObject inGameAttack1 = Instantiate(enemy, player.transform.position, player.transform.rotation);
     }
+
+    
 }
