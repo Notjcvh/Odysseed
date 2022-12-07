470502cc20bf1b2051c7b459ca60a42588574ab7
SueMin Lim <32788609+jessica3146@users.noreply.github.com>
2022-12-07 16:07:43 -0500

    Menu_2nd try

--- My project/Assets/PauseMenu.cs
+++ My project/Assets/PauseMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GamePaused = false;
     public GameObject pauseMenuUI;
 
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -19,15 +20,17 @@ public class PauseMenu : MonoBehaviour
             else
             {
                 Pause();
+                Cursor.visible = true;
             }
         }
     }
 
-    void Resume()
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GamePaused = false;
+        Cursor.visible = false;
     }
 
     void Pause()
@@ -35,5 +38,17 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GamePaused = true;
+        Cursor.visible = true;
+    }
+
+    public void LoadSetting()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Setting");
+    }
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
